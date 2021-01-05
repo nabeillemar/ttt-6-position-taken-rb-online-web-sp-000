0d7ba1447c523f49147292441c25814b08f71b6c
Nabeil Lemar <nabeillemar@gmail.com>
2021-01-05 00:55:30 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,7 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = 0
+
+
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
